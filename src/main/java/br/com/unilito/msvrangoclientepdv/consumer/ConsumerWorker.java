945ba0a92a0b94da46b359efdbc6a21fad946e14
Uniliva <uniliva.pereira@confitec.com.br>
2020-09-16 00:17:48 -0300

    Creando consumer

---
+++ src/main/java/br/com/unilito/msvrangoclientepdv/consumer/ConsumerWorker.java
@@ -0,0 +1,17 @@
+package br.com.unilito.msvrangoclientepdv.consumer;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Slf4j
+public class ConsumerWorker {
+	
+	@KafkaListener(topics = "${topic.kafka.pedido}", groupId = "${topic.kafka.grupo.id}")
+	public void consumer(String message) {
+		log.info("Consumindo: {}", message);
+	}
+
+}
