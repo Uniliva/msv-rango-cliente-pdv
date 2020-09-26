df77a7da8dd8b86ec799877a794c34b50359eb94
Uniliva <uniliva.pereira@confitec.com.br>
2020-09-25 22:26:32 -0300

    Ajustando consumer

--- src/main/java/br/com/unilito/msvrangoclientepdv/consumer/ConsumerWorker.java
+++ src/main/java/br/com/unilito/msvrangoclientepdv/consumer/ConsumerWorker.java
@@ -8,7 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 @Slf4j
 public class ConsumerWorker {
-	
+
 	@KafkaListener(topics = "${topic.kafka.pedido}", groupId = "${topic.kafka.grupo.id}")
 	public void consumer(String message) {
 		log.info("Consumindo: {}", message);
